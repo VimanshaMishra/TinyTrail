d623c0ca068ba1057f4984528df2741418652884
Vimansha Mishra <vimansha@Vimanshas-MacBook-Air.local>
2026-03-28 00:57:23 +0530

    initial commit

---
+++ src/main/java/com/tini/trail/TinitrailApplication.java
@@ -0,0 +1,13 @@
+package com.tini.trail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinitrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinitrailApplication.class, args);
+	}
+
+}
