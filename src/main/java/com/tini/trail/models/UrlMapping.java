1be5540f4a810d333e4a8003a0cf63f12e796446
Vimansha Mishra <vimansha@Vimanshas-MacBook-Air.local>
2026-04-02 00:40:44 +0530

    adding userdetailsimp

---
+++ src/main/java/com/tini/trail/models/UrlMapping.java
@@ -0,0 +1,24 @@
+package com.tini.trail.models;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity @Data
+
+
+public class UrlMapping {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalurl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+
+    private User user;
+    @OneToMany (mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+}
---
+++ src/main/java/com/tini/trail/services/UserDetailImp.java
@@ -0,0 +1,58 @@
+package com.tini.trail.services;
+
+import com.tini.trail.models.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserDetailImp implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Collection<? extends GrantedAuthority > authorities;
+
+    public UserDetailImp(Long id, String username, String password, String email, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authorities = authorities;
+    }
+    public static UserDetailImp build (User user) {
+        GrantedAuthority auhtority= new SimpleGrantedAuthority("ROLE_USER");
+        return new UserDetailImp(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getEmail(),
+                Collections.singletonList(auhtority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
