850e867053609a7b9d6d2b785850e69b9e7fd808
Vimansha Mishra <vimanshamishra28@gmail.com>
2026-04-03 16:38:05 +0530

    Reversed the changes

--- src/main/java/com/tini/trail/services/UserDetailImp.java
+++ src/main/java/com/tini/trail/services/UserDetailImp.java
@@ -55,8 +55,6 @@ public class UserDetailImp implements UserDetails {
     @Override
     public String getUsername() {
         System.out.println("Username: " + username);
-        System.out.println("test"  + username);
-        System.out.println("test2"  + username);
         return username  ;
 
     }
